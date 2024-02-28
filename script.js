78299d485a8ef0f4fd7cea82d9973931d1c03191
nocive.science <nocive.science@gmail.com>
2024-02-28 03:25:39 -0300

    la nave se mueve y las balas se comportan correctamente

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-ctx.scale(.5, .5);
+const escala=.25;
+ctx.scale(escala, escala);
+
+var shootLasers= false;
+var dx = 0;
+var dy = 0;
 
 var angle = 0;
 var x = canvas.width / 2;
@@ -12,8 +17,27 @@ img.src = 'ship.png';
 var keys = {};
 var bullets = [];
 
+function moveForward() {
+    x += 5 * Math.cos(angle * Math.PI / 180-Math.PI/2);
+    y += 5 * Math.sin(angle * Math.PI / 180-Math.PI/2);
+    if (x > canvas.width/escala) {
+        x = 0;
+    }else if(x < 0){
+        x = canvas.width/escala;
+    }
+    if (y > canvas.height/escala) {
+        y = 0;
+    }else if(y < 0){
+        y = canvas.height/escala;
+    }
+}
+
+function moveShip() {
+    x+=dx;
+    y+=dy;
+}
+
 function drawShip() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.save();
     ctx.translate(x, y);
     ctx.rotate(angle * Math.PI / 180);
@@ -26,22 +50,35 @@ function drawBullets(){
         var bullet = bullets[i];
         bullet.x += bullet.dx;
         bullet.y += bullet.dy;
+        ctx.save();
         ctx.beginPath();
         ctx.arc(bullet.x, bullet.y, 5, 0, Math.PI * 2, false);
         ctx.fillStyle = 'red';
         ctx.fill();
         ctx.closePath();
+        ctx.restore();
     }
 }
 
 function fire() {
-    var bullet = {
-        x: x+150*Math.cos(angle * Math.PI / 180-Math.PI/2),
-        y: y+150*Math.sin(angle * Math.PI / 180-Math.PI/2),
-        dx: 50 * Math.cos(angle * Math.PI / 180-Math.PI/2),
-        dy: 50 * Math.sin(angle * Math.PI / 180-Math.PI/2)
-    };
-    bullets.push(bullet);
+    if(bullets.length < 10){
+        var bullet = {
+            x: x+150*Math.cos(angle * Math.PI / 180-Math.PI/2),
+            y: y+150*Math.sin(angle * Math.PI / 180-Math.PI/2),
+            dx: 50 * Math.cos(angle * Math.PI / 180-Math.PI/2),
+            dy: 50 * Math.sin(angle * Math.PI / 180-Math.PI/2)
+        };
+        bullets.push(bullet);
+    }else{
+        bullets.shift(); //remove the first bullet
+        var bullet = {
+            x: x+150*Math.cos(angle * Math.PI / 180-Math.PI/2),
+            y: y+150*Math.sin(angle * Math.PI / 180-Math.PI/2),
+            dx: 50 * Math.cos(angle * Math.PI / 180-Math.PI/2),
+            dy: 50 * Math.sin(angle * Math.PI / 180-Math.PI/2)
+        };
+        bullets.push(bullet);
+    }
     // var radian = angle * Math.PI / 180;
     // bullets.push({
     //     x: x,
@@ -64,15 +101,17 @@ document.onkeydown = document.onkeyup = function(e) {
 };
 
 var animate = function() {
+    ctx.clearRect(0, 0, canvas.width/escala, canvas.height/escala);
     if (keys['a']) rotateLeft();
     if (keys['d']) rotateRight();
     if (keys['p']) fire();
+    if (keys['w']) moveForward();
+    moveShip();
     drawShip();
     drawBullets();
     requestAnimationFrame(animate);
 };
 
 img.onload = function() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
     animate();
 };
